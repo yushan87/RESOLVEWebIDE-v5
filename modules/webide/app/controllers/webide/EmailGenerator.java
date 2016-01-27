825ee1bed194333191bb687ef9dae46bab58c16d
Yu-Shan Sun <yushans@g.clemson.edu>
2016-01-26 23:40:03 -0500

    Added scala templates for the emails

--- modules/webide/app/controllers/webide/EmailGenerator.java
+++ modules/webide/app/controllers/webide/EmailGenerator.java
@@ -5,6 +5,7 @@ import models.common.database.User;
 import play.libs.mailer.Email;
 import play.libs.mailer.MailerClient;
 import play.mvc.Http;
+import views.html.webide.email.reset;
 
 /**
  * TODO: Add JavaDocs for this class.
@@ -24,43 +25,32 @@ public class EmailGenerator {
     // ===========================================================
 
     public void generateWelcomeEmail(User user) {
-        Email email = generateEmailObject(user.email);
-        /*setSubject("Welcome to RESOLVE Web IDE");
-        addRecipient(user.email);
-        setFrom("Clemson RSRG <do_not_reply@" + Http.Request.current().host + ">");
-
+        /*Email email = generateEmailObject(user.email, "Welcome to RESOLVE Web IDE", "");
         String link = Http.Request.current().getBase() + (String)Play.configuration.get("http.path") + "/";
         send(user, link);*/
     }
 
     public void generateConfirmationEmail(User user) {
-        Email email = generateEmailObject(user.email);
-        /*String confirmationCode = user.confirmationCode;
-        setSubject("RESOLVE Web IDE Registration Confirmation");
-        addRecipient(user.email);
-        setFrom("Clemson RSRG <do_not_reply@" + Http.Request.current().host + ">");
+        /*Email email = generateEmailObject(user.email, "RESOLVE Web IDE Registration Confirmation");
+        String confirmationCode = user.confirmationCode;
 
         String link = Http.Request.current().getBase() + (String)Play.configuration.get("http.path") + "/confirm?c_code=" + confirmationCode + "&email=" + user.email;
         send(user, link);*/
     }
 
     public void lostPassword(User user) {
-        Email email = generateEmailObject(user.email);
-        /*String confirmationCode = user.confirmationCode;
-        setSubject("RESOLVE Web IDE Password Recovery");
-        addRecipient(user.email);
-        setFrom("Clemson RSRG <do_not_reply@" + Http.Request.current().host + ">");
+        /*Email email = generateEmailObject(user.email, "RESOLVE Web IDE Password Recovery");
+        String confirmationCode = user.confirmationCode;
 
         String link = Http.Request.current().getBase() + (String)Play.configuration.get("http.path") + "/reset?c_code=" + confirmationCode + "&email=" + user.email;
         send(user, link);*/
     }
 
     public void resetPassword(User user) {
-        Email email = generateEmailObject(user.email);
-        /*setSubject("RESOLVE Web IDE Password Reset");
-        addRecipient(user.email);
-        setFrom("Clemson RSRG <do_not_reply@" + Http.Request.current().host + ">");
-        send(user);*/
+        String userEmail = user.email;
+        String firstName = user.firstName;
+        Email email = generateEmailObject(user.email, "RESOLVE Web IDE Password Reset", reset.render(firstName, userEmail).body());
+        myMailerClient.send(email);
     }
 
     // ===========================================================
@@ -71,13 +61,17 @@ public class EmailGenerator {
      * <p>An helper method to create an email object.</p>
      *
      * @param emailAddress The recipient's email address.
+     * @param subject The subject of the email.
+     * @param body The body of the email. This must be in HTML format.
      *
      * @return A new email object with the sender and recipient information.
      */
-    private Email generateEmailObject(String emailAddress) {
+    private Email generateEmailObject(String emailAddress, String subject, String body) {
         Email email = new Email();
         email.addTo(emailAddress);
         email.setFrom("Clemson RSRG <do_not_reply@" + Http.Context.current().request().host() + ">");
+        email.setSubject(subject);
+        email.setBodyHtml(body);
 
         return email;
     }
