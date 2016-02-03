b1ca3d0ad24a6a17a37feaf85a6ecf7b584ef0d2
Yu-Shan Sun <yushans@g.clemson.edu>
2016-02-03 13:24:20 -0500

    Added a registration page to the routes

--- modules/common/app/controllers/common/authentication/Registration.java
+++ modules/common/app/controllers/common/authentication/Registration.java
@@ -1,8 +1,37 @@
 package controllers.common.authentication;
 
+import controllers.common.utilities.CachedObjects;
+import play.db.jpa.Transactional;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.common.authentication.registration;
+
+import javax.inject.Inject;
+
 /**
  * TODO: Add JavaDocs for this class.
  */
-public class Registration {
-    
+public class Registration extends Controller {
+
+    // ===========================================================
+    // Global Variables
+    // ===========================================================
+
+    /** <p>Collection of cached objects</p> */
+    @Inject
+    private CachedObjects myCachedObjects;
+
+    // ===========================================================
+    // Public Methods
+    // ===========================================================
+
+    /**
+     * <p>This renders the registration page for the WebIDE.</p>
+     *
+     * @return The result of rendering the page
+     */
+    @Transactional(readOnly = true)
+    public Result index() {
+        return ok(registration.render(myCachedObjects.getProjects(), myCachedObjects.getDefaultProject()));
+    }
 }
