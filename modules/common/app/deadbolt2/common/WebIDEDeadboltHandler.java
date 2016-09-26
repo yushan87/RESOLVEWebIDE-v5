47fd6bec286cc34db42f90add8a4a3e0049b42f5
Yu-Shan Sun <yushans@g.clemson.edu>
2016-09-26 13:11:39 -0400

    Added a blank implementation of deadbolt handler

---
+++ modules/common/app/deadbolt2/common/WebIDEDeadboltHandler.java
@@ -0,0 +1,112 @@
+package deadbolt2.common;
+
+import be.objectify.deadbolt.java.AbstractDeadboltHandler;
+import be.objectify.deadbolt.java.DeadboltHandler;
+import be.objectify.deadbolt.java.DynamicResourceHandler;
+import be.objectify.deadbolt.java.ExecutionContextProvider;
+import be.objectify.deadbolt.java.models.Permission;
+import be.objectify.deadbolt.java.models.Subject;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+import play.mvc.Http;
+import play.mvc.Result;
+
+/**
+ * <p>An implementation of {@link DeadboltHandler} that handles authorization
+ * for the entire RESOLVE WebIDE application.</p>
+ *
+ * @author Yu-Shan Sun
+ * @version 1.0
+ */
+public class WebIDEDeadboltHandler extends AbstractDeadboltHandler {
+
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+
+    public WebIDEDeadboltHandler(ExecutionContextProvider ecProvider) {
+        super(ecProvider);
+    }
+
+    // ===========================================================
+    // Public Methods
+    // ===========================================================
+
+    /**
+     * <p>Invoked immediately before controller or view restrictions are checked.
+     * This forms the integration with any authentication actions that
+     * may need to occur.</p>
+     *
+     * @param context the HTTP context
+     *
+     * @return the action result if an action other than the delegate must be taken,
+     * otherwise null. For a case where the user is authenticated (or whatever your
+     * test condition is), this will be null otherwise the restriction won't be applied.
+     */
+    @Override
+    public CompletionStage<Optional<Result>> beforeAuthCheck(Http.Context context) {
+        return null;
+    }
+
+    /**
+     * <p>Gets the current {@link Subject}, e.g. the current user.</p>
+     *
+     * @param context the HTTP context
+     *
+     * @return the current subject
+     */
+    @Override
+    public CompletionStage<Optional<? extends Subject>> getSubject(Http.Context context) {
+        return null;
+    }
+
+    /**
+     * <p>Invoked when an access failure is detected on <i>controllerClassName</i>.</p>
+     *
+     * @param context the HTTP context
+     * @param content the content type hint. This can be used to return a response
+     *                in the appropriate content type, e.g. JSON.
+     *
+     * @return the action result
+     */
+    @Override
+    public CompletionStage<Result> onAuthFailure(Http.Context context, Optional<String> content) {
+        return null;
+    }
+
+    /**
+     * <p>Gets the handler used for dealing with resources restricted to specific users/groups.</p>
+     *
+     * @param context the HTTP context
+     *
+     * @return the handler for restricted resources. May be null.
+     */
+    @Override
+    public CompletionStage<Optional<DynamicResourceHandler>> getDynamicResourceHandler(Http.Context context) {
+        return null;
+    }
+
+    /**
+     * <p>Gets the canonical name of the handler. Defaults to the class name.</p>
+     *
+     * @return whatever the implementor considers the canonical name of the handler to be.
+     */
+    @Override
+    public String handlerName() {
+        return null;
+    }
+
+    /**
+     * <p>Get the permissions associated with a role.</p>
+     *
+     * @param roleName the role the permissions are associated with.
+     *
+     * @return a non-null list containing the permissions associated with the role.
+     */
+    @Override
+    public CompletionStage<List<? extends Permission>> getPermissionsForRole(String roleName) {
+        return null;
+    }
+
+}
