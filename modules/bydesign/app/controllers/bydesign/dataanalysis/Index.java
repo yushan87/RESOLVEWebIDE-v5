7f8ba4405cd60e04d635f48557ce7b7aa717e992
Yu-Shan Sun <yushans@g.clemson.edu>
2016-10-29 15:41:01 -0400

    Added a basic form to upload the data and a controller to receive it

--- modules/bydesign/app/controllers/bydesign/dataanalysis/Index.java
+++ modules/bydesign/app/controllers/bydesign/dataanalysis/Index.java
@@ -1,8 +1,13 @@
 package controllers.bydesign.dataanalysis;
 
 import models.common.database.User;
+import java.io.File;
 import play.db.jpa.Transactional;
+import play.filters.csrf.AddCSRFToken;
+import play.filters.csrf.RequireCSRFCheck;
 import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
 import views.html.bydesign.dataanalysis.index;
 
@@ -16,10 +21,11 @@ public class Index extends Controller {
     // ===========================================================
 
     /**
-     * <p>This renders the byDesign page for the WebIDE.</p>
+     * <p>This renders the {@code byDesign} index page.</p>
      *
      * @return The result of rendering the page
      */
+    @AddCSRFToken
     @Transactional(readOnly = true)
     public Result index() {
         // Retrieve the current user (if logged in)
@@ -31,6 +37,30 @@ public class Index extends Controller {
         }
 
         return redirect(controllers.common.security.routes.Security.index());
+    }
+
+    /**
+     * <p>This handles file upload that contains the {@code byDesign}
+     * user IDs.</p>
+     *
+     * @return The result of rendering the page.
+     */
+    @AddCSRFToken
+    @RequireCSRFCheck
+    public Result upload() {
+        MultipartFormData<File> body = request().body().asMultipartFormData();
+        FilePart<File> idFile = body.getFile("idFile");
+        if (idFile != null) {
+            String fileName = idFile.getFilename();
+            String contentType = idFile.getContentType();
+            File file = idFile.getFile();
+
+            return ok("File uploaded");
+        } else {
+            flash("error", "Missing file");
+
+            return badRequest();
+        }
     }
 
 }
