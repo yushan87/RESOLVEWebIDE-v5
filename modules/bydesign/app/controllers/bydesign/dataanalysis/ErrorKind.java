aa66d317a8fc3b21d92949e2e4cc4cea1c4415f7
Yu-Shan Sun <yushans@g.clemson.edu>
2016-11-09 15:49:09 -0500

    Added an error enumeration class

---
+++ modules/bydesign/app/controllers/bydesign/dataanalysis/ErrorKind.java
@@ -0,0 +1,57 @@
+package controllers.bydesign.dataanalysis;
+
+/**
+ * <p>This file contains the various different errors we might encounter during
+ * data analysis and it's corresponding string message to the user.</p>
+ *
+ * @author Yu-Shan Sun
+ * @version 1.0
+ */
+public enum ErrorKind {
+
+    // ===========================================================
+    // Different Error Codes
+    // ===========================================================
+
+    /**
+     * <p>Error 1: Invalid CSRF Token.</p>
+     */
+    INVALID_CSRF_TOKEN(1, "Invalid CSRF Token."),
+
+    /**
+     * <p>Error 2: Invalid Input File.</p>
+     */
+    INVALID_INPUT_FILE(2, "The input file is not a valid CSV file."),
+
+    /**
+     * <p>Error 3: Error Retrieving Data from the Database.</p>
+     */
+    DATABASE_ERROR(3, "An error occurred while retrieving data from the database.");
+
+    // ===========================================================
+    // Global Variables
+    // ===========================================================
+
+    /** <p>Error code</p> */
+    public final int code;
+
+    /** <p>Message associated with the error</p> */
+    public final String message;
+
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+
+    /**
+     * <p>Helper constructor that allow us to create the various different
+     * {@link ErrorKind}.</p>
+     *
+     * @param code Error code.
+     * @param message Message associated with this error.
+     */
+    ErrorKind(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+}
