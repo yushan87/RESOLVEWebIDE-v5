209cb81920a5951ca180769f007b0ef1d5dd5d1b
Yu-Shan Sun <yushans@g.clemson.edu>
2016-11-27 11:46:09 -0500

    All headers!

--- modules/common/app/controllers/common/passwordrecovery/PasswordRecovery.java
+++ modules/common/app/controllers/common/passwordrecovery/PasswordRecovery.java
@@ -1,3 +1,14 @@
+/**
+ * ---------------------------------
+ * Copyright (c) 2016
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
 package controllers.common.passwordrecovery;
 
 import com.fasterxml.jackson.databind.JsonNode;
--- modules/common/app/models/common/database/CompilerResult.java
+++ modules/common/app/models/common/database/CompilerResult.java
@@ -1,3 +1,14 @@
+/**
+ * ---------------------------------
+ * Copyright (c) 2016
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
 package models.common.database;
 
 import javax.persistence.*;
